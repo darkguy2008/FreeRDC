7bafc74b0056c48b7108ce1815bd0cdc70012b81
DARKGuy <darkguy2008@gmail.com>
2016-05-30 15:50:21 -0600

    CommandBase WIP

---
+++ Windows/FreeRDC.Network/CommandConnection.cs
@@ -0,0 +1,29 @@
+﻿using SharpRUDP;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace FreeRDC.Network
+{
+    public class CommandConnection
+    {
+        public RUDPConnection Connection { get; set; }
+
+        private static JavaScriptSerializer _js = new JavaScriptSerializer();
+
+        public void Server(string address, int port)
+        {
+            Connection.OnPacketReceived += EvtPacketReceived;
+            Connection.Listen(address, port);
+        }
+
+        public void Client(string address, int port)
+        {
+            Connection.Connect(address, port);
+        }
+
+        private void EvtPacketReceived(RUDPPacket p)
+        {
+            Command c = _js.Deserialize<Command>(Encoding.ASCII.GetString(p.Data));
+        }
+    }
+}
